308651d0fc53bd7b2db224db42ea2d35d5f6ba74
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-06-30 22:11:53 +0430

    Create Comments.py

---
+++ Python/Core/Comments.py
@@ -0,0 +1,29 @@
+# Creating a Comment
+
+#%%  Example
+# This is a comment.
+print("Hello, World!")
+
+#%%  Example
+print("Hello, World!")  # This is a comment
+
+#%%  Example
+# print("Hello, World!")
+print("Cheers, Mate!")
+
+
+# Multi Line Comments
+
+#%%  Example
+# This is a comment
+# written in
+# more than just one line
+print("Hello, World!")
+
+#%%  Example
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
