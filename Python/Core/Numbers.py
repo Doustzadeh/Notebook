39758e2defcce7b1a682ca9a52b00adad0aff76f
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-01 16:18:51 +0430

    Create Numbers.py

---
+++ Python/Core/Numbers.py
@@ -0,0 +1,88 @@
+# Python Numbers
+
+# %% Example
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Int
+
+# %% Example
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Float
+
+# %% Example
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# %% Example
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Complex
+
+# %% Example
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Type Conversion
+
+# %% Example
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# Random Number
+
+# %% Example
+import random
+
+print(random.randrange(1, 10))
