5e821417d5dffc93bed944958d6a787358febf71
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-01 10:30:56 +0430

    Create Variables.py

---
+++ Python/Core/Variables.py
@@ -0,0 +1,46 @@
+# Creating Variables
+
+#%% Example
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#%% Example
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+# Casting
+
+#%% Example
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+# Get the Type
+
+#%% Example
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+# Single or Double Quotes
+
+#%% Example
+x = "John"
+# is the same as
+x = 'John'
+
+
+# Case-Sensitive
+# Variable names are case-sensitive.
+
+#%%  Example
+a = 4
+A = "Sally"
+# A will not overwrite a
