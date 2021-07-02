393684849dee116a23d736738ffb5fa501738726
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-02 22:01:58 +0430

    Add region

--- Python/Core/Casting.py
+++ Python/Core/Casting.py
@@ -1,17 +1,21 @@
+# region Casting
+
 # Specify a Variable Type
 
 # %% Example
-x = int(1)   # x will be 1
-y = int(2.8) # y will be 2
-z = int("3") # z will be 3
+x = int(1)    # x will be 1
+y = int(2.8)  # y will be 2
+z = int("3")  # z will be 3
 
 # %% Example
-x = float(1)     # x will be 1.0
-y = float(2.8)   # y will be 2.8
-z = float("3")   # z will be 3.0
-w = float("4.2") # w will be 4.2
+x = float(1)      # x will be 1.0
+y = float(2.8)    # y will be 2.8
+z = float("3")    # z will be 3.0
+w = float("4.2")  # w will be 4.2
 
 # %% Example
-x = str("s1") # x will be 's1'
-y = str(2)    # y will be '2'
-z = str(3.0)  # z will be '3.0'
+x = str("s1")  # x will be 's1'
+y = str(2)     # y will be '2'
+z = str(3.0)   # z will be '3.0'
+
+# endregion
