42059ec51d09f66bf988b527c227e0e128b746b2
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-05 14:23:37 +0430

    Add some examples

--- Python/Core/Booleans.py
+++ Python/Core/Booleans.py
@@ -43,4 +43,69 @@ print(bool(x))
 print(bool(y))
 # Out: True
 
+
+# Most Values are True
+# Almost any value is evaluated to True if it has some sort of content.
+# Any string is True, except empty strings.
+# Any number is True, except 0.
+# Any list, tuple, set, and dictionary are True, except empty ones.
+
+# %% Example
+# The following will return True:
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+# Out: True
+
+
+# Some Values are False
+
+# %% Example
+# The following will return False:
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+# Out: False
+
+# %% Example
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+# Out: False
+
+
+# Functions can Return a Boolean
+
+# %% Example
+# Print the answer of a function:
+def myFunction() :
+  return True
+
+print(myFunction())
+# Out: True
+
+# %% Example
+# Print "YES!" if the function returns True, otherwise print "NO!":
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+# Out: YES!
+
+# %% Example
+# Check if an object is an integer or not:
+x = 200
+print(isinstance(x, int))
+# Out: True
+
 # endregion
