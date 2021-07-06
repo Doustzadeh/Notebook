32a305c6c00215c40160224046af7ed9e41b186b
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-06 16:08:45 +0430

    Add "region Change List Items"

--- Python/Core/Lists.py
+++ Python/Core/Lists.py
@@ -125,3 +125,51 @@ if "apple" in thislist:
 # Out: Yes, 'apple' is in the fruits list
 
 # endregion
+
+
+# region Change List Items
+
+# Change Item Value
+
+# %% Example
+# Change the second item:
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+# Out: ['apple', 'blackcurrant', 'cherry']
+
+
+# Change a Range of Item Values
+
+# %% Example
+# Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+# Out: ['apple', 'blackcurrant', 'watermelon', 'orange', 'kiwi', 'mango']
+
+# %% Example
+# Change the second value by replacing it with two new values:
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+# Out: ['apple', 'blackcurrant', 'watermelon', 'cherry']
+
+# %% Example
+# Change the second and third value by replacing it with one value:
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+# Out: ['apple', 'watermelon']
+
+
+# Insert Items
+
+# %% Example
+# Insert "watermelon" as the third item:
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+# Out: ['apple', 'banana', 'watermelon', 'cherry']
+
+# endregion
