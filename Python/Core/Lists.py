9e149c4fe1c64babd7be831f951b74297f5d220b
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-08 01:45:30 +0430

    Add "region Join Lists"

--- Python/Core/Lists.py
+++ Python/Core/Lists.py
@@ -539,3 +539,38 @@ print(mylist)
 # Out: ['apple', 'banana', 'cherry']
 
 # endregion
+
+
+# region Join Lists
+
+# ---------------------------------------------------------
+# Join Two Lists
+
+# %% Example
+# Join two list:
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+list3 = list1 + list2
+print(list3)
+# Out: ['a', 'b', 'c', 1, 2, 3]
+
+# %% Example
+# Append list2 into list1:
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+# Out: ['a', 'b', 'c', 1, 2, 3]
+
+# %% Example
+# Use the extend() method to add list2 at the end of list1:
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+list1.extend(list2)
+print(list1)
+# Out: ['a', 'b', 'c', 1, 2, 3]
+
+# endregion
