9477b985b5db1345774d7f5941b5f52574a37e59
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-08 20:45:25 +0430

    Add "region Access Tuple Items"

--- Python/Core/Tuples.py
+++ Python/Core/Tuples.py
@@ -65,3 +65,72 @@ print(thistuple)
 # Out: ('apple', 'banana', 'cherry')
 
 # endregion
+
+
+# region Access Tuple Items
+
+# ---------------------------------------------------------
+# Access Tuple Items
+# Note: The first item has index 0.
+
+# %% Example
+# Print the second item in the tuple:
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+# Out: banana
+
+
+# ---------------------------------------------------------
+# Negative Indexing
+# Negative indexing means start from the end.
+
+# %% Example
+# Print the last item of the tuple:
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+# Out: cherry
+
+
+# ---------------------------------------------------------
+# Range of Indexes
+
+# %% Example
+# Return the third, fourth, and fifth item:
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+# Out: ('cherry', 'orange', 'kiwi')
+
+# %% Example
+# This example returns the items from the beginning to, but NOT included, "kiwi":
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+# Out: ('apple', 'banana', 'cherry', 'orange')
+
+# %% Example
+# This example returns the items from "cherry" and to the end:
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+# Out: ('cherry', 'orange', 'kiwi', 'melon', 'mango')
+
+
+# ---------------------------------------------------------
+# Range of Negative Indexes
+
+# %% Example
+# This example returns the items from index -4 (included) to index -1 (excluded)
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+# Out: ('orange', 'kiwi', 'melon')
+
+
+# ---------------------------------------------------------
+# Check if Item Exists
+
+# %% Example
+# Check if "apple" is present in the tuple:
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+    print("Yes, 'apple' is in the fruits tuple")
+# Out: Yes, 'apple' is in the fruits tuple
+
+# endregion
