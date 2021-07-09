6d7d2c2d439ae1318ec754e177d5ecd9464f357f
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-09 22:44:26 +0430

    Add "region Update Tuples"

--- Python/Core/Tuples.py
+++ Python/Core/Tuples.py
@@ -134,3 +134,65 @@ if "apple" in thistuple:
 # Out: Yes, 'apple' is in the fruits tuple
 
 # endregion
+
+
+# region Update Tuples
+
+# ---------------------------------------------------------
+# Change Tuple Values
+# Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+
+# %% Example
+# Convert the tuple into a list to be able to change it:
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
+# Out: ('apple', 'kiwi', 'cherry')
+
+
+# ---------------------------------------------------------
+# Add Items
+# Since tuples are immutable, they do not have a build-in append() method, but there are other ways to add items to a tuple.
+
+# %% Example
+# Convert the tuple into a list, add "orange", and convert it back into a tuple:
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)
+# Out: ('apple', 'banana', 'cherry', 'orange')
+
+# %% Example
+# Create a new tuple with the value "orange", and add that tuple:
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+print(thistuple)
+# Out: ('apple', 'banana', 'cherry', 'orange')
+
+
+# ---------------------------------------------------------
+# Remove Items
+# Note: You cannot remove items in a tuple.
+
+# %% Example
+# Convert the tuple into a list, remove "apple", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+print(thistuple)
+# Out: ('banana', 'cherry')
+
+# %% Example
+# The del keyword can delete the tuple completely:
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
+# Out: NameError: name 'thistuple' is not defined
+
+# endregion
