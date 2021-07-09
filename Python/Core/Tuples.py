5a659a6c1804fbd51f80a77c881a62c89599537a
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-09 22:55:14 +0430

    Add "region Loop Tuples"

--- Python/Core/Tuples.py
+++ Python/Core/Tuples.py
@@ -247,3 +247,48 @@ print(red)
 # Out: cherry
 
 # endregion
+
+
+# region Loop Tuples
+
+# ---------------------------------------------------------
+# Loop Through a Tuple
+
+# %% Example
+# Iterate through the items and print the values:
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+    print(x)
+# Out: apple
+#      banana
+#      cherry
+
+
+# ---------------------------------------------------------
+# Loop Through the Index Numbers
+
+# %% Example
+# Print all items by referring to their index number:
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+   print(thistuple[i])
+# Out: apple
+#      banana
+#      cherry
+
+
+# ---------------------------------------------------------
+# Using a While Loop
+
+# %% Example
+# Print all items, using a while loop to go through all the index numbers:
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+    print(thistuple[i])
+    i = i + 1
+# Out: apple
+#      banana
+#      cherry
+
+# endregion
