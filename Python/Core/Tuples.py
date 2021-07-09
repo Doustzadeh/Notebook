64d28333d2790fb92931e61bedb8f49d308a8c23
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-09 22:58:14 +0430

    Add "region Join Tuples"

--- Python/Core/Tuples.py
+++ Python/Core/Tuples.py
@@ -292,3 +292,30 @@ while i < len(thistuple):
 #      cherry
 
 # endregion
+
+
+# region Join Tuples
+
+# ---------------------------------------------------------
+# Join Two Tuples
+
+# %% Example
+# Join two tuples:
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+# Out: ('a', 'b', 'c', 1, 2, 3)
+
+
+# ---------------------------------------------------------
+# Multiply Tuples
+
+# %% Example
+# Multiply the fruits tuple by 2:
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+print(mytuple)
+# Out: ('apple', 'banana', 'cherry', 'apple', 'banana', 'cherry')
+
+# endregion
