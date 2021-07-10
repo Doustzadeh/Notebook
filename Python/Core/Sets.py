be134e19749bc210ab8984c60d94ad1a49b0df05
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-10 15:43:24 +0430

    Add "region Add Set Items"

--- Python/Core/Sets.py
+++ Python/Core/Sets.py
@@ -91,3 +91,42 @@ print("banana" in thisset)
 # Out: True
 
 # endregion
+
+
+# region Add Set Items
+
+# ---------------------------------------------------------
+# Add Items
+
+# %% Example
+# Add an item to a set, using the add() method:
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+# Out: {'banana', 'cherry', 'apple', 'orange'}
+
+
+# ---------------------------------------------------------
+# Add Sets
+
+# %% Example
+# Add elements from tropical into thisset:
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset)
+# Out: {'banana', 'pineapple', 'apple', 'papaya', 'cherry', 'mango'}
+
+
+# ---------------------------------------------------------
+# Add Any Iterable
+
+# %% Example
+# Add elements of a list to at set:
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
+# Out: {'apple', 'cherry', 'kiwi', 'banana', 'orange'}
+
+# endregion
