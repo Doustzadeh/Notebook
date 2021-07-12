e4bcca5eb730e778203dc08a026d13e66196a8bc
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-12 18:03:27 +0430

    Add some examples

--- Python/Core/Dictionaries.py
+++ Python/Core/Dictionaries.py
@@ -269,3 +269,100 @@ print(thisdict)
 # Out: {'brand': 'Ford', 'model': 'Mustang', 'year': 2020}
 
 # endregion
+
+
+# region Add Dictionary Items
+
+# ---------------------------------------------------------
+# Adding Items
+
+# %% Example
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+# Out: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
+
+
+# ---------------------------------------------------------
+# Update Dictionary
+
+# %% Example
+# Add a color item to the dictionary by using the update() method:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
+print(thisdict)
+# Out: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
+
+# endregion
+
+
+# region Remove Dictionary Items
+
+# ---------------------------------------------------------
+# Removing Items
+
+# %% Example
+# The pop() method removes the item with the specified key name:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+# Out: {'brand': 'Ford', 'year': 1964}
+
+# %% Example
+# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+# Out: {'brand': 'Ford', 'model': 'Mustang'}
+
+# %% Example
+# The del keyword removes the item with the specified key name:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+# Out: {'brand': 'Ford', 'year': 1964}
+
+# %% Example
+# The del keyword can also delete the dictionary completely:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists.
+# Out: NameError: name 'thisdict' is not defined
+
+# %% Example
+# The clear() method empties the dictionary:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+# Out: {}
+
+# endregion
