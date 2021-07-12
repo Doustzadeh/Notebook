ee9c84d20572ffdc654bf784cce20df58f691873
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-12 17:24:17 +0430

    Add "region Access Dictionary Items"

--- Python/Core/Dictionaries.py
+++ Python/Core/Dictionaries.py
@@ -80,3 +80,158 @@ print(type(thisdict))
 # Out: <class 'dict'>
 
 # endregion
+
+
+# region Access Dictionary Items
+
+# ---------------------------------------------------------
+# Accessing Items
+
+# %% Example
+# Get the value of the "model" key:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+# Out: Mustang
+
+# %% Example
+# Get the value of the "model" key:
+x = thisdict.get("model")
+print(x)
+# Out: Mustang
+
+
+# ---------------------------------------------------------
+# Get Keys
+
+# %% Example
+# Get a list of the keys:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict.keys()
+print(x)
+# Out: dict_keys(['brand', 'model', 'year'])
+
+# %% Example
+# Add a new item to the original dictionary, and see that the keys list gets updated as well:
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.keys()
+print(x) # before the change
+# Out: dict_keys(['brand', 'model', 'year'])
+car["color"] = "white"
+print(x) # after the change
+# Out: dict_keys(['brand', 'model', 'year', 'color'])
+
+
+# ---------------------------------------------------------
+# Get Values
+
+# %% Example
+# Get a list of the values:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict.values()
+print(x)
+# Out: dict_values(['Ford', 'Mustang', 1964])
+
+# %% Example
+# Make a change in the original dictionary, and see that the values list gets updated as well:
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.values()
+print(x) # before the change
+# Out: dict_values(['Ford', 'Mustang', 1964])
+car["year"] = 2020
+print(x) # after the change
+# Out: dict_values(['Ford', 'Mustang', 2020])
+
+# %% Example
+# Add a new item to the original dictionary, and see that the values list gets updated as well:
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.values()
+print(x) #before the change
+# Out: dict_values(['Ford', 'Mustang', 1964])
+car["color"] = "red"
+print(x) #after the change
+# Out: dict_values(['Ford', 'Mustang', 1964, 'red'])
+
+
+# ---------------------------------------------------------
+# Get Items
+
+# %% Example
+# Get a list of the key:value pairs
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict.items()
+print(x)
+# Out: dict_items([('brand', 'Ford'), ('model', 'Mustang'), ('year', 1964)])
+
+# %% Example
+# Make a change in the original dictionary, and see that the items list gets updated as well:
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.items()
+print(x) #before the change
+# Out: dict_items([('brand', 'Ford'), ('model', 'Mustang'), ('year', 1964)])
+car["year"] = 2020
+print(x) #after the change
+# Out: dict_items([('brand', 'Ford'), ('model', 'Mustang'), ('year', 2020)])
+
+# %% Example
+# Add a new item to the original dictionary, and see that the items list gets updated as well:
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.items()
+print(x) #before the change
+# Out: dict_items([('brand', 'Ford'), ('model', 'Mustang'), ('year', 1964)])
+car["color"] = "red"
+print(x) #after the change
+# Out: dict_items([('brand', 'Ford'), ('model', 'Mustang'), ('year', 1964), ('color', 'red')])
+
+
+# ---------------------------------------------------------
+# Check if Key Exists
+
+# %% Example
+# Check if "model" is present in the dictionary:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+    print("Yes, 'model' is one of the keys in the thisdict dictionary")
+# Out: Yes, 'model' is one of the keys in the thisdict dictionary
+
+# endregion
