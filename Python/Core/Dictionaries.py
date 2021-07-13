d84980c723aa10637607201a20b2051301f104d1
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-13 18:29:12 +0430

    Add some examples

--- Python/Core/Dictionaries.py
+++ Python/Core/Dictionaries.py
@@ -439,3 +439,82 @@ for x, y in thisdict.items():
 #      year 1964
 
 # endregion
+
+
+# region Copy Dictionaries
+
+# ---------------------------------------------------------
+# Copy a Dictionary
+
+# %% Example
+# Make a copy of a dictionary with the copy() method:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+# Out: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
+
+# %% Example
+# Make a copy of a dictionary with the dict() function:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+# Out: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
+
+# endregion
+
+
+# region Nested Dictionaries
+
+# ---------------------------------------------------------
+# Nested Dictionaries
+
+# %% Example
+# Create a dictionary that contain three dictionaries:
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(myfamily)
+# Out: {'child1': {'name': 'Emil', 'year': 2004}, 'child2': {'name': 'Tobias', 'year': 2007}, 'child3': {'name': 'Linus', 'year': 2011}}
+
+# %% Example
+# Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+print(myfamily)
+# Out: {'child1': {'name': 'Emil', 'year': 2004}, 'child2': {'name': 'Tobias', 'year': 2007}, 'child3': {'name': 'Linus', 'year': 2011}}
+
+# endregion
