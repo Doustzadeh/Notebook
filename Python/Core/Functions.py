8b3bc10ffc6b6b2f401b5a9c9f47a664352e74c6
Mostafa Doustzadeh <m.doustzadeh@hotmail.com>
2021-07-15 23:00:11 +0430

    Add some examples

--- Python/Core/Functions.py
+++ Python/Core/Functions.py
@@ -49,4 +49,66 @@ def my_function(fname, lname):
 my_function("Emil")
 # Out: TypeError: my_function() missing 1 required positional argument: 'lname'
 
+
+# ---------------------------------------------------------
+# Arbitrary Arguments, *args
+
+# %% Example
+# If the number of arguments is unknown, add a * before the parameter name:
+def my_function(*kids):
+    print("The youngest child is " + kids[2])
+my_function("Emil", "Tobias", "Linus")
+# Out: The youngest child is Linus
+
+
+# ---------------------------------------------------------
+# Keyword Arguments
+
+# %% Example
+def my_function(child3, child2, child1):
+    print("The youngest child is " + child3)
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+# Out: The youngest child is Linus
+
+
+# ---------------------------------------------------------
+# Arbitrary Keyword Arguments, **kwargs
+
+# %% Example
+# If the number of keyword arguments is unknown, add a double ** before the parameter name:
+def my_function(**kid):
+    print("His last name is " + kid["lname"])
+my_function(fname = "Tobias", lname = "Refsnes")
+# Out: His last name is Refsnes
+
+
+# ---------------------------------------------------------
+# Default Parameter Value
+
+# %% Example
+def my_function(country = "Norway"):
+    print("I am from " + country)
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+# Out: I am from Sweden
+#      I am from India
+#      I am from Norway
+#      I am from Brazil
+
+
+# ---------------------------------------------------------
+# Passing a List as an Argument
+
+# %% Example
+def my_function(food):
+    for x in food:
+        print(x)
+fruits = ["apple", "banana", "cherry"]
+my_function(fruits)
+# Out: apple
+#      banana
+#      cherry
+
 # endregion
