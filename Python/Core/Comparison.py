b9574de61836b24538a6877c34cd40678c026dc4
Doustzadeh <m.doustzadeh@hotmail.com>
2021-12-13 17:43:23 +0330

    Create Comparison.py

---
+++ Python/Core/Comparison.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------
+# Comparison Operators
+
+# %% Example
+print(10 > 3)
+# Out: True
+print(10 == "10")
+# Out: False
+print("bag" > "apple")
+# Out: True
+print("bag" > "BAG")
+# Out: True
+print(ord("b"))
+# Out: 98
+print(ord("B"))
+# Out: 66
